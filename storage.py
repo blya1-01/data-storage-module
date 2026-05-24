86277edff1cc74092546300f8c1cc6b24f893e50
Антон Ким <2008antonkim@gmail.com>
2026-05-24 16:11:18 +0300

    Add storage.py code

---
+++ storage.py
@@ -0,0 +1,10 @@
+def save_history(entry):
+    with open("history.txt", "a") as f:
+        f.write(entry + "\n")
+
+def load_history():
+    try:
+        with open("history.txt", "r") as f:
+            return [line.strip() for line in f.readlines()]
+    except:
+        return []
